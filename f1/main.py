648fd1163a0fd1e6e99abed18bcb27f712c295cd
TheophileBERTELOOT <thber64@ulaval.ca>
2025-02-14 11:07:10 -0500

    sample script REALLLLLLLLLLLLLY basic

--- f1/main.py
+++ f1/main.py
@@ -1,7 +1,23 @@
-
+from libemg import streamers, data_handler, filtering, gui, emg_predictor, feature_extractor, utils
+import pyautogui
 
 def cli():
-    print('hello world')
+    streamer, smm = streamers.sifi_biopoint_streamer(name='BioPoint_v1_3', 
+                                                    ecg=True, 
+                                                    imu=True, 
+                                                    ppg=True, 
+                                                    eda=True, 
+                                                    emg=True,
+                                                    filtering=True,
+                                                    emg_notch_freq=60)
+    odh = data_handler.OnlineDataHandler(smm)
+    
+    while True:
+        data = odh.get_data(1)
+        if data['Inertie'][0] < 50:
+            pyautogui.press('right')
+        elif data['Inertie'][0] < 50:
+            pyautogui.press('left')
 
 if __name__ == "__main__":
     cli()

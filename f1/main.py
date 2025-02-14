26b851afdd7d9f48287ff3d318c3fdf58381724d
Deoth <gueideoth@gmail.com>
2025-02-14 14:09:39 -0500

    map control

--- f1/main.py
+++ f1/main.py
@@ -1,5 +1,9 @@
 from libemg import streamers, data_handler, filtering, gui, emg_predictor, feature_extractor, utils
-import pyautogui
+import keyboard
+import vgamepad as vg 
+import time
+gamepad = vg.VX360Gamepad()
+
 
 def cli():
     streamer, smm = streamers.sifi_biopoint_streamer(name='BioPoint_v1_3', 
@@ -13,11 +17,19 @@ def cli():
     odh = data_handler.OnlineDataHandler(smm)
     
     while True:
-        data = odh.get_data(1)
-        if data['Inertie'][0] < 50:
-            pyautogui.press('right')
-        elif data['Inertie'][0] < 50:
-            pyautogui.press('left')
-
+        data = odh.get_data(2)
+        
+        if data[0]['imu'][0][0] < 5000000000:
+            #pyautogui.press('right')
+            gamepad.right_trigger(255)
+            gamepad.update()
+            time.sleep(0.1)
+        if data[0]['imu'][0][1] < 500000000000:
+            #pyautogui.press('left')
+            gamepad.left_joystick(x_value=-32000 , y_value= 0)
+            gamepad.update()
+            time.sleep(0.1)
+        
+            
 if __name__ == "__main__":
     cli()

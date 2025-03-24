f9d340dfe0448cb52b43706f23b86bd86fd46917
Deoth <gueideoth@gmail.com>
2025-03-24 15:34:42 -0400

    Update gesture list and enhance GUI functionality; modify gesture definitions and implement data collection features

--- f1/main.py
+++ f1/main.py
@@ -3,6 +3,8 @@ from libemg import streamers, data_handler, filtering, gui, emg_predictor, featu
 import keyboard
 import vgamepad as vg 
 import time
+import math
+import numpy as np
 
 from drive import accelerate, brake, steer_left, steer_right, reset_controls
 gamepad = vg.VX360Gamepad()
@@ -18,31 +20,39 @@ def cli():
                                                     filtering=True,
                                                     emg_notch_freq=60)
     odh = data_handler.OnlineDataHandler(smm)
-    
+    i=0
+    avg=0
+    buffer_size = 10
     while True:
         data = odh.get_data(2)
-
+        if i == buffer_size:
+            i=0
+            print(np.floor(avg/buffer_size))
+            print("-----------------------------")
+            avg=0
+        avg+=data[0]['imu'][0]
+        i+=1
         
-        print(data[0]['imu'][0])
-        print("-----------------------------")
+        # print(data[0]['imu'][0])
+        # print("-----------------------------")
         
-        if data[0]['imu'][0][0] >= 6.55:
+        if data[0]['imu'][0][0] <= -7:
             #pyautogui.press('right')
             #gamepad.right_trigger(255)
             #gamepad.update()
 
             accelerate()
-        if data[0]['imu'][0][0] <= -6.55:
+        if data[0]['imu'][0][0] >= 7:
             #pyautogui.press('right')
             #gamepad.right_trigger(255)
             #gamepad.update()
 
             brake()
 
-        if data[0]['imu'][0][2] >= 7.55:
+        if data[0]['imu'][0][2] >= 2:
             
             steer_right()
-        if data[0]['imu'][0][2] <= -7.55:
+        if data[0]['imu'][0][2] <= -2:
             steer_left()
            
         else:

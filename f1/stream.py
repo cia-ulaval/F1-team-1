48e7d58f31ea102917bbc459afeac416e2d0f971
Deoth <gueideoth@gmail.com>
2025-03-28 13:21:55 -0400

    Fix data collection parameters and remove unused GUI class

--- f1/stream.py
+++ f1/stream.py
@@ -35,7 +35,7 @@ def preparemodel():
     """
 
     odh = data_handler.OfflineDataHandler()
-    for i in range(0, 8):
+    for i in range(0, 7):
         odh.get_data(folder_location="data/S" + str(i) + "/", regex_filters= regex_filters)
     windows, metadata = odh.parse_windows(WINDOW_SIZE,WINDOW_INC)
 
@@ -141,7 +141,7 @@ def collectdata():
     
     args = {
         "media_folder": "images/",
-        "data_folder": "data/S" + str("0") + "/",
+        "data_folder": "data/S" + str("4") + "/",
         "num_reps": 6,
         "rep_time": 5,
         "rest_time": 3,
@@ -161,140 +161,3 @@ if __name__ == "__main__":
         testband()
     if STAGE == 3:
         testmodel()
-
-class GUI:
-    """
-    The Screen Guided Training module. 
-
-    By default, this module has two purposes: 
-    (1) Launching a Screen Guided Training window. 
-    (2) Downloading gesture sets from our library of gestures located at:
-    https://github.com/libemg/LibEMGGestures
-
-    Parameters
-    ----------
-    online_data_handler: OnlineDataHandler
-        Online data handler used for acquiring raw EMG data.
-    args: dic, default={'media_folder': 'images/', 'data_folder':'data/', 'num_reps': 3, 'rep_time': 5, 'rest_time': 3, 'auto_advance': True}
-        The dictionary that defines the SGT window. Keys are: 'media_folder', 
-        'data_folder', 'num_reps', 'rep_time', 'rest_time', and 'auto_advance'. All media (i.e., images and videos) in 'media_folder' will be played in alphabetical order.
-        For video files, a matching labels file of the same name will be searched for and added to the 'data_folder' if found.
-        'rep_time' is only used for images since the duration of videos is automatically calculated based on
-        the number of frames (assumed to be 24 FPS).  
-    width: int, default=1920
-        The width of the SGT window. 
-    height: int, default=1080
-        The height of the SGT window.
-    gesture_width: int, default=500
-        The width of the embedded gesture image/video.
-    gesture_height: int, default=500
-        The height of the embedded gesture image/video.
-    clean_up_on_call: Boolean, default=False
-        If true, this will cleanup (and kill) the streamer reference.
-    """
-    def __init__(self, 
-                 online_data_handler,
-                 args={'media_folder': 'images/', 'data_folder':'data/', 'num_reps': 3, 'rep_time': 5, 'rest_time': 3, 'auto_advance': True},
-                 width=1920,
-                 height=1080,
-                 debug=False,
-                 gesture_width = 500,
-                 gesture_height = 500,
-                 clean_up_on_kill=False):
-        
-        self.width = width 
-        self.height = height 
-        self.debug = debug
-        self.online_data_handler = online_data_handler
-        self.args = args
-        self.video_player_width = gesture_width
-        self.video_player_height = gesture_height
-        self.clean_up_on_kill = clean_up_on_kill
-        self._install_global_fields()
-
-    def start_gui(self):
-        """
-        Launches the Screen Guided Training UI.
-        """
-        self._window_init(self.width, self.height, self.debug)
-        
-    def _install_global_fields(self):
-        # self.global_fields = ['offline_data_handlers', 'online_data_handler']
-        self.offline_data_handlers = []   if 'offline_data_handlers' not in self.args.keys() else self.args["offline_data_handlers"]
-        self.offline_data_aliases  = []   if 'offline_data_aliases'  not in self.args.keys() else self.args["offline_data_aliases"]
-
-    def _window_init(self, width, height, debug=False):
-        dpg.create_context()
-        dpg.create_viewport(title="LibEMG",
-                            width=width,
-                            height=height)
-        dpg.setup_dearpygui()
-        
-
-        self._file_menu_init()
-
-        dpg.show_viewport()
-        dpg.set_exit_callback(self._on_window_close)
-
-        if debug:
-            dpg.configure_app(manual_callback_management=True)
-            while dpg.is_dearpygui_running():
-                jobs = dpg.get_callback_queue()
-                dpg.run_callbacks(jobs)
-                dpg.render_dearpygui_frame()
-        else:
-            dpg.start_dearpygui()
-        dpg.destroy_context()
-
-    def _file_menu_init(self):
-
-        with dpg.viewport_menu_bar():
-            with dpg.menu(label="File"):
-                dpg.add_menu_item(label="Exit")
-                
-            with dpg.menu(label="Data"):
-                dpg.add_menu_item(label="Collect Data", callback=self._data_collection_callback)
-                #dpg.add_menu_item(label="Import Data",  callback=self._import_data_callback )
-                #dpg.add_menu_item(label="Export Data",  callback=self._export_data_callback)
-                #dpg.add_menu_item(label="Inspect Data", callback=self._inspect_data_callback)
-            
-            # with dpg.menu(label="Visualize"):
-                # dpg.add_menu_item(label="Live Signal", callback=self._visualize_livesignal_callback)
-            
-            # with dpg.menu(label="Model"):
-                # dpg.add_menu_item(label="Train Classifier", callback=self._train_classifier_callback)
-
-            # with dpg.menu(label="HCI"):
-                # dpg.add_menu_item(label="Fitts Law", callback=self._fitts_law_callback)
-
-    def _data_collection_callback(self):
-        panel_arguments = list(inspect.signature(DataCollectionPanel.__init__).parameters)
-        passed_arguments = {i: self.args[i] for i in self.args.keys() if i in panel_arguments}
-        self.dcp = DataCollectionPanel(**passed_arguments, gui=self, video_player_width=self.video_player_width, video_player_height=self.video_player_height)
-        self.dcp.spawn_configuration_window()
-
-    def _import_data_callback(self):
-        panel_arguments = list(inspect.signature(DataImportPanel.__init__).parameters)
-        passed_arguments = {i: self.args[i] for i in self.args.keys() if i in panel_arguments}
-        self.dip = DataImportPanel(**passed_arguments, gui=self)
-        self.dip.spawn_configuration_window()
-
-    def _export_data_callback(self):
-        pass
-
-    def _inspect_data_callback(self):
-        pass
-
-    def _train_classifier_callback(self):
-        pass
-
-    def _fitts_law_callback(self):
-        pass
-
-    def _on_window_close(self):
-        if self.clean_up_on_kill:
-            print("Window is closing. Performing clean-up...")
-            if 'streamer' in self.args.keys():
-                self.args['streamer'].signal.set()
-            time.sleep(3)
-
